24ec5f26eba9814c0315fc053421087575ba583d
khemmachart.r@apricotion.com <khemmachart.r@apricotion.com>
2013-07-19 23:21:43 +0700

    index

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -1,10 +1,10 @@
 <?=$this->load->view('includes/inc-header','', TRUE)?>
 	<?=$view?>
-<div id="common-popup-container">
+<!--div id="common-popup-container">
 	<div id="common-popup">
 		<div id="content"></div>
 		<input type="checkbox" name="chk-dont-show-popup" id="chk-dont-show-popup" />
 		<a href="#" id="b-close-common-popup"></a>
 	</div>
-</div>
+</div-->
 <?=$this->load->view('includes/inc-footer','', TRUE)?>
