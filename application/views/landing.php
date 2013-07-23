87f8b1266997f558248782987c22468e8179fce3
supa-aon <supa-aon@hotmail.com>
2013-07-23 05:34:59 -0700

    add new landing page

---
+++ application/views/landing.php
@@ -0,0 +1,11 @@
+<style type="text/css">
+	#member-sub-menu { display:none; }
+	
+	#container { background: transparent url("") no-repeat; }
+</style>
+<div id="content-body" class="page-home">
+
+	<div id="content">
+		<div id="box"></div>
+	</div>
+</div>
