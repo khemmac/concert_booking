9dc695acfcbda0a2679de1428ae06e7354972649
khemmachart.r@apricotion.com <khemmachart.r@apricotion.com>
2013-08-06 16:47:30 +0700

    edit seat css

--- application/views/includes/seat/s1d.php
+++ application/views/includes/seat/s1d.php
@@ -1,37 +1,4 @@
 <style type="text/css">
 	#seat-container { top:-4px; left:112px; width:598px; height:606px; }
-	.row { width:613px; left:55px; }
-	.row-j { top:282px; }
-	.row-k { top:258px; }
-	.row-l { top:234px; }
-	.row-m { top:210px; }
-	.row-n { top:185px; }
-	.row-p { top:161px; }
-	.row-q { top:137px; }
-	.row-r { top:112px; }
-	.row-t { top:64px; }
-	.row-s { top:89px; }
-	.row-u { top:40px; }
-	.row-v { top:17px; }
-
-	.pos-1 { left:1px; }
-	.pos-2 { left:33px; }
-	.pos-3 { left:64px; }
-	.pos-4 { left:95px; }
-	.pos-5 { left:126px; }
-	.pos-6 { left:157px; }
-	.pos-7 { left:188px; }
-	.pos-8 { left:220px; }
-	.pos-9 { left:251px; }
-	.pos-10 { left:282px; }
-	.pos-11 { left:313px; }
-	.pos-12 { left:345px; }
-	.pos-13 { left:375px; }
-	.pos-14 { left:407px; }
-	.pos-15 { left:438px; }
-	.pos-16 { left:469px; }
-	.pos-17 { left:500px; }
-	.pos-18 { left:531px; }
-	.pos-19 { left:562px; }
-	.pos-20 { left:593px; }
+	#chair-container { left: 54px; top: 15px; }
 </style>
