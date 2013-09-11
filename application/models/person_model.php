d948a2ce8aa9b57ae461d1c923ef42ec496099a3
khemmachart.r@apricotion.com <khemmachart.r@apricotion.com>
2013-09-12 03:57:09 +0700

    lang for validator

--- application/models/person_model.php
+++ application/models/person_model.php
@@ -1,6 +1,12 @@
 <?php
 Class Person_model extends CI_Model
 {
+	public function __construct() {
+		parent::__construct();
+		//$this->lang->load("message","english");
+	}
+
+
 	function login($username, $password){
 		$this->db->select('id, username, thName, enName');
 		$this->db->where('username', $username);
